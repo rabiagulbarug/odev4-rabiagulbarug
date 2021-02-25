437c0347f6621ae3f49a7d9cffb849f2fa2efd3a
Rabia Gül Baruğ <66168375+rabiagulbarug@users.noreply.github.com>
2021-02-25 14:36:34 +0300

    Update SQL.php

--- SQL.php
+++ SQL.php
@@ -1,3 +1,18 @@
 <?php
 
+ abstract class SQL{
+    use PDOConnector;
+    public bool $connectionStatus = false;
 
+    public abstract function create(SQLQuery $query);
+    public abstract function update(SQLQuery $query);
+    public abstract function delete(SQLQuery $query);
+
+    public function setParams(array $params){
+
+    }
+    public function isConnected(){
+
+    }
+
+}
