484ef5fc4a5a36a6302b9daf9c6b50801c612f65
Rabia Gül Baruğ <66168375+rabiagulbarug@users.noreply.github.com>
2021-02-25 14:33:37 +0300

    Update PDOConnector.php

--- PDOConnector.php
+++ PDOConnector.php
@@ -1,3 +1,17 @@
 <?php
+    trait PDOConnector{
+        public function connect(){
+
+        }
+        public function disconnect(){
+
+        }
+        protected function setParams(array $params){
+
+        }
+    }
+
+
+
 
 
