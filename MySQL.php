92cd3a60a93679144604336c5adf69b83526494c
Rabia Gül Baruğ <66168375+rabiagulbarug@users.noreply.github.com>
2021-02-25 14:31:57 +0300

    Update MySQL.php

--- MySQL.php
+++ MySQL.php
@@ -1,3 +1,15 @@
 <?php
 
+include "IQuery.php";
+include "PDOConnector.php";
+include "SQLQuery.php";
+include "SQL.php";
+include "SQLCrud.php";
+include "MySQL.php";
+include "PostgreSQL.php";
 
+$db = new MySQL("localhost", "root", "toor", "database");
+
+$query = (new SQLQuery())->setTable("users")->select()->addWhere("username", "=", ":user")->addBinding("user", "eray");
+
+$eray = $db->first($query);
